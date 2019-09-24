97f7e65469b9938b54f6f83d0ebfbcc32e3ec113
Shane Fonyi <sfonyi@ACIs-MacBook-Pro.local>
2019-09-24 17:56:26 -0400

    add main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "dictionary.h"
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#define DICTIONARY "wordlist.txt"
+#define TESTDICT "test_worlist.txt"
+
+int main(int argc, char *argv[])
+{
+
+}
+
