8a849b0bfe0740627517ac36fc7fb23ba8f5c686
Shane Fonyi <shane@jayhawk.us>
2019-10-23 20:34:26 -0400

    Update main.c

--- main.c
+++ main.c
@@ -3,15 +3,6 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-void printMisspelled(char* misspelled[]) {
-    int i;
-    for(i = 0; i < MAX_MISSPELLED; i++) {
-        if(misspelled[i] != NULL) {
-            printf("Incorrect: %s \n", misspelled[i]);
-        }
-    }
-}
-
 int main(int argc, char *argv[])
 {
     hashmap_t hashtable[HASH_SIZE];
@@ -24,6 +15,5 @@ int main(int argc, char *argv[])
     num_misspelled = check_words(fp, hashtable, misspelled);
     fclose(fp);
     printf("There are %d mistakes in the file.\n",num_misspelled);
-    printMisspelled();
 }
 
